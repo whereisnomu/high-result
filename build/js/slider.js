f89670fb088ec05a10467c15855a04afc7fe4926
Arthur Chagirov <multidevelop.java@gmail.com>
2023-12-14 02:07:38 +0300

    25:00 hours completed package franshise & slider ga .!

--- build/js/slider.js
+++ build/js/slider.js
@@ -1,5 +1,3 @@
-// new Splide(".splide").mount();
-
 new Splide(".splide", {
   arrows: "true",
   type: "loop",
@@ -8,4 +6,18 @@ new Splide(".splide", {
   rewindSpeed: 5000,
   speed: 2500,
   gap: "30px",
+  breakpoints: {
+    768: {
+      width: "600px",
+      height: "420px",
+    },
+    480: {
+      width: "600px",
+      height: "500px",
+    },
+    360: {
+      width: "600px",
+      height: "590px",
+    },
+  },
 }).mount();
